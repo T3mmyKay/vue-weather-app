4b7ddf14428e05d272476a2e1b91204a1cdb7ecd
T3mmyKay <103121606+T3mmyKay@users.noreply.github.com>
2022-10-28 22:43:25 +0100

    changes

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -2,7 +2,7 @@
     <header class="sticky top-0 bg-weather-primary shadow-lg">
         <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
             <RouterLink :to="{name:'home'}">
-                <div class="flex items-center gap-3"><i class="fa fa-solid fa-sun text-2xl text-yellow-400"></i>
+                <div class="flex items-center gap-3"><i class="fa fa-solid fa-sun text-2xl text-yellow-300"></i>
                     <p class="text-2xl">The Local Weather</p>
                 </div>
             </RouterLink>
