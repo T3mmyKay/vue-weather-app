e19f434c38158e4c160ba07b8a52fb4683b99aec
Komolafe Temmy <temmyk7@gmail.com>
2023-06-15 19:01:58 +0100

    changes

--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -3,7 +3,7 @@
         <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
             <RouterLink :to="{name:'home'}">
                 <div class="flex items-center gap-3"><i class="fa fa-solid fa-sun text-2xl text-yellow-300"></i>
-                    <p class="text-2xl">The Local Weather</p>
+                        <p class="text-2xl">T3mmy Weather</p>
                 </div>
             </RouterLink>
 
